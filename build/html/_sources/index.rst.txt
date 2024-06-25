28f1f405826a3141a2c94b9348d38d8e4948ed97
Kamal Raj <kamal.raj2181@gmail.com>
2024-06-25 23:40:22 +0530

    adding setup files

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,22 @@
+.. Doc as a code documentation master file, created by
+   sphinx-quickstart on Wed Jun 20 11:30:32 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Doc as a code!
+=========================================
+
+Let's rock the doc as a code.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   about
+   prerequisites
+
+
+Indices and tables
+==================
+
+* :ref:`search`
---
+++ source/conf.py
@@ -0,0 +1,44 @@
+# conf.py
+
+import furo
+
+project = 'doc-as-a-code'
+copyright = '2024, Kamal Raj'
+author = 'Kamal Raj'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_versions',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+# Theme settings
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#4A4A4A",
+        "color-brand-content": "#4A4A4A",
+    },
+    "dark_css_variables": {
+        "color-background-primary": "#2E2E2E",
+        "color-background-secondary": "#353535",
+        "color-foreground-primary": "#E0E0E0",
+        "color-foreground-secondary": "#C0C0C0",
+        "color-brand-primary": "#B0B0B0",
+        "color-brand-content": "#B0B0B0",
+    },
+}
+
+# Versions configuration
+versions_branch_name = 'main'
+versions_base_url = 'https://github.com/kamalraj21/docs-as-a-code'  # Replace with your GitHub repository name if different
+versions = ['v1.0', 'v2.0', 'v3.0']  # Replace with your actual version names
